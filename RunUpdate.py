c08c510300e0bfaa5df57f450bff4836fc497663
Pavel Vlha <pvlha@redhat.com>
2023-11-09 14:48:45 +0100

    Added error handling for invalid GitHub IDs

--- RunUpdate.py
+++ RunUpdate.py
@@ -28,33 +28,37 @@ try:
         firstIndex = issue.fields.summary.find("#") + 1
         lastIndex = issue.fields.summary.find("]")
         response = requests.get("{}{}{}/issues/{}".format(config.gitBaseUrl, config.gitHubOrgName, "/keycloak", issue.fields.summary[firstIndex:lastIndex]))
-        logging.debug("GitHub request succesfull")
-        data = json.loads(response.text)
-        # Processing response to the defined Python object for further processing
-        ghIssue = IntegratedIssue(data["id"], data["title"], data["body"], data["number"], data["labels"], data["state"], data["assignee"], data["assignees"], data["html_url"], config.areaMappers)
-        #Preparing JIRA object for update
-        jiraIssue = {
-                        'project': {'key':config.jiraProject},
-                        'summary': "[{}{}{}] {}".format(config.jiraGitPrefix, ghIssue.number, config.jiraGitSuffix, ghIssue.title),
-                        'description' : str(ghIssue.body),
-                        'customfield_12313240' : str(ghIssue.team),
-                        'labels' : ghIssue.resolveLabels([]),
-                    }
-        jiraIssue["labels"] = ghIssue.resolveLabels(issue.fields.labels)
-        # Checking, if the link to the original GitHub Issue is already in JIRA, otherwise adding it. This needs to be done to avoiding duplicated links
-        issueGitHubLink = config.gitHubLinkBaseUrl.format(config.gitHubOrgName, config.gitHubRepository, ghIssue.number)
-        links = jira.remote_links(issue)
-        hasLink = False
-        for link in links:
-            if link.object.url == issueGitHubLink:
-                hasLink = True
-        if not hasLink:
-            jira.add_simple_link(issue, {"url":issueGitHubLink, "title":"Link to original GitHub issue"})
-        #Updating JIRA issue
-        issue.update(jiraIssue)
-        if issue.fields.status.name != ghIssue.status:
-            jira.transition_issue(issue, config.statusMapper[ghIssue.status])
-        logging.info("GitHub issue number {} succesfully checked under JIRA issue {}".format(ghIssue.number, issue.key))
+        if response.status_code != 200:
+            logging.error("GitHub Issue #{} not found. Check the GitHub issue number if correct.".format(issue.fields.summary[firstIndex:lastIndex]))
+        else:
+            logging.debug("GitHub Issue found")
+            data = json.loads(response.text)
+            # Processing response to the defined Python object for further processing
+            ghIssue = IntegratedIssue(data["id"], data["title"], data["body"], data["number"], data["labels"], data["state"], data["assignee"], data["assignees"], data["html_url"], config.areaMappers)
+            #Preparing JIRA object for update
+            jiraIssue = {
+                            'project': {'key':config.jiraProject},
+                            'summary': "[{}{}{}] {}".format(config.jiraGitPrefix, ghIssue.number, config.jiraGitSuffix, ghIssue.title),
+                            'description' : str(ghIssue.body),
+                            'customfield_12313240' : str(ghIssue.team),
+                            'labels' : ghIssue.resolveLabels([]),
+                        }
+            jiraIssue["labels"] = ghIssue.resolveLabels(issue.fields.labels)
+            # Checking, if the link to the original GitHub Issue is already in JIRA, otherwise adding it. This needs to be done to avoiding duplicated links
+            issueGitHubLink = config.gitHubLinkBaseUrl.format(config.gitHubOrgName, config.gitHubRepository, ghIssue.number)
+            links = jira.remote_links(issue)
+            hasLink = False
+            for link in links:
+                if link.object.url == issueGitHubLink:
+                    hasLink = True
+            if not hasLink:
+                jira.add_simple_link(issue, {"url":issueGitHubLink, "title":"Link to original GitHub issue"})
+            #Updating JIRA issue
+            issue.update(jiraIssue)
+            if issue.fields.status.name != ghIssue.status:
+                jira.transition_issue(issue, config.statusMapper[ghIssue.status])
+            logging.info("GitHub issue number {} succesfully updated under JIRA issue {}".format(ghIssue.number, issue.key))
+    jira.close()
     logging.info("Integration run succesfull!")
 
 except Exception as error:
